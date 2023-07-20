c390f8db1e9113e1a42b57db40dab28cf52e9868
Hitoshi Mikamie <mikamin1025@gmail.comm>
2023-07-20 22:46:51 +0900

    done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { client } from "../libs/client";
 import styles from "@/styles/Home.module.scss";
+import ConvertDate from "../libs/convertdate";
 
 //SSGを使ってデータを取得
 export const getStaticProps = async () => {
@@ -14,12 +15,15 @@ export const getStaticProps = async () => {
 };
 
 export default function Home({ blog }) {
+  console.log(blog);
   return (
     <div className={styles.container}>
+      <h1>Blog</h1>
       {blog.map((blog) => (
-        <li key={blog.id}>
+        <li key={blog.id} className={styles.post_content}>
           <Link href={`blog/${blog.id}`}>
-          {blog.title}
+            <h2 className={styles.post_content_text}>{blog.title}</h2>
+            <p className={styles.post_content_text}><ConvertDate convertDate={blog.createdAt}></ConvertDate></p>
             {/* <a href="/">{blog.title}</a> */}
           </Link>
         </li>
